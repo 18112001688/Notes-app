b0522535d3cf9c2ef28281bdcc74806e2f0f0c76
cipher <moustafasaper167@gmail.com>
2023-04-01 01:59:30 +0200

    another screen created sucessfully which is edit note

--- lib/views/widgets/note_item.dart
+++ lib/views/widgets/note_item.dart
@@ -1,57 +1,66 @@
 import 'package:flutter/material.dart';
 
+import 'edit_note_body.dart';
+
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: const Color(0xffffcc80),
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter tips',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 26,
-              ),
-            ),
-            subtitle: const Padding(
-              padding: EdgeInsets.only(top: 20.0),
-              child: Text(
-                'Flutter is a cross platform for building mobile apps',
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return const EditNoteViewBody();
+        }));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: const Color(0xffffcc80),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter tips',
                 style: TextStyle(
-                  color: Colors.black54,
-                  fontSize: 18,
+                  color: Colors.black,
+                  fontSize: 26,
+                ),
+              ),
+              subtitle: const Padding(
+                padding: EdgeInsets.only(top: 20.0),
+                child: Text(
+                  'Flutter is a cross platform for building mobile apps',
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                  size: 28,
+                  color: Colors.black,
                 ),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-                size: 28,
-                color: Colors.black,
+            const Padding(
+              padding: EdgeInsets.only(right: 16.0),
+              child: Text(
+                'May21, 2023',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                ),
               ),
-            ),
-          ),
-          const Padding(
-            padding: EdgeInsets.only(right: 16.0),
-            child: Text(
-              'May21, 2023',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 12,
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
