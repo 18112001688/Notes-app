571ee529a8097d59ad63b617b24bdf2c6e82cd61
cipher <moustafasaper167@gmail.com>
2023-04-13 10:18:06 +0200

    take the text fields data and vlidate it

--- lib/views/widgets/add_note_sheet.dart
+++ lib/views/widgets/add_note_sheet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/constant/constant.dart';
 import 'package:notes_app/views/widgets/coustem_field.dart';
 
 import 'button.dart';
@@ -9,28 +8,77 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
       child: SingleChildScrollView(
-        child: Column(
-          children: const [
-            SizedBox(
-              height: 32,
-            ),
-            CoustemTextField(hintText: 'Title'),
-            SizedBox(
-              height: 16,
-            ),
-            CoustemTextField(hintText: 'content', maxLines: 5),
-            SizedBox(
-              height: 30,
-            ),
-            AddButton(),
-            SizedBox(
-              height: 20,
-            ),
-          ],
-        ),
+        child: AddNotesForm(),
+      ),
+    );
+  }
+}
+
+class AddNotesForm extends StatefulWidget {
+  const AddNotesForm({
+    super.key,
+  });
+
+  @override
+  State<AddNotesForm> createState() => _AddNotesFormState();
+}
+
+class _AddNotesFormState extends State<AddNotesForm> {
+  // key which can be used later to access the FormState object and interact with the form fields
+  GlobalKey<FormState> formkey = GlobalKey();
+  // check the vldation of the form
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  String? title;
+  String? subtitle;
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: formkey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 32,
+          ),
+          CoustemTextField(
+            hintText: 'Title',
+            onSaved: (value) {
+              title = value;
+            },
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          CoustemTextField(
+              hintText: 'content',
+              maxLines: 5,
+              onSaved: (value) {
+                subtitle = value;
+              }),
+          const SizedBox(
+            height: 30,
+          ),
+          AddButton(
+            onPressed: () {
+              // make the vldation when tab the add button
+              if (formkey.currentState!.validate()) {
+                formkey.currentState!.save();
+              } else {
+                //is an enum property that determines how the form fields should be automatically validated. Setting it to , it change the color of the form red to green
+                // as you begain weitting
+                autovalidateMode = AutovalidateMode.always;
+                setState(() {});
+              }
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+        ],
       ),
     );
   }
