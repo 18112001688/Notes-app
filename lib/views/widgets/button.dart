e5734566c930c53b0f02618e0b4e66a13721cdb7
cipher <moustafasaper167@gmail.com>
2023-04-13 10:18:52 +0200

    adding on pressed and on saved to the form field and button

--- lib/views/widgets/button.dart
+++ lib/views/widgets/button.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant/constant.dart';
 
 class AddButton extends StatelessWidget {
-  const AddButton({super.key});
-
+  const AddButton({super.key, required this.onPressed});
+  final VoidCallback onPressed;
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
-      onPressed: () {},
+      onPressed: onPressed,
       color: primaryColor,
       minWidth: MediaQuery.of(context).size.width,
       height: 50,
--- lib/views/widgets/coustem_field.dart
+++ lib/views/widgets/coustem_field.dart
@@ -3,12 +3,22 @@ import 'package:notes_app/constant/constant.dart';
 
 class CoustemTextField extends StatelessWidget {
   const CoustemTextField(
-      {super.key, required this.hintText, this.maxLines = 1});
+      {super.key, required this.hintText, this.maxLines = 1, this.onSaved});
   final String hintText;
   final int maxLines;
+  final Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      onSaved: onSaved,
+      validator: (value) {
+        // check if the value is null
+        if (value?.isEmpty ?? true) {
+          return 'Field is required';
+        } else {
+          return null;
+        }
+      },
       cursorColor: primaryColor,
       maxLines: maxLines,
       decoration: InputDecoration(
