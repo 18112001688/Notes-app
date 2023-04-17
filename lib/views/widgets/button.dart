34d14c9db89479cbd325f0132c1a93da666dde98
cipher <moustafasaper167@gmail.com>
2023-04-17 03:38:09 +0200

    change add note loading

--- lib/views/widgets/button.dart
+++ lib/views/widgets/button.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/constant/constant.dart';
 
 class AddButton extends StatelessWidget {
-  const AddButton({super.key, required this.onPressed});
+  const AddButton({super.key, required this.onPressed, this.isLoading = false});
   final VoidCallback onPressed;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
@@ -12,14 +13,23 @@ class AddButton extends StatelessWidget {
       color: primaryColor,
       minWidth: MediaQuery.of(context).size.width,
       height: 50,
-      child: const Text(
-        'Add',
-        style: TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
+      child: isLoading
+          ? const SizedBox(
+              height: 24,
+              width: 24,
+              child: CircularProgressIndicator(
+                color: Colors.black,
+                strokeWidth: 2,
+              ),
+            )
+          : const Text(
+              'Add',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
     );
   }
 }
